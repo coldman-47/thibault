6e4d69f984ecbb8a01dbbf64d332c3d9df2f7562
ouldmahammedhamou <hamou.ouldmahammed@gmail.com>
2024-10-25 11:53:34 +0200

    j'ai fini ma partir restaurant

--- src/app/restaurant/restaurant.page.ts
+++ src/app/restaurant/restaurant.page.ts
@@ -25,41 +25,41 @@ export class RestaurantPage implements OnInit {
     {
       title: 'Le Malabar Brasserie',
       img: 'https://a.mktgcdn.com/p/30sNtv09F8yrHk4_guhw8geSPuXJZIQ7rPCdWEQlL-k/3000x2250.jpg',
-      price: 18,
+      price: '',
       description: 'situé au 75007  vous accueille dans une , situé au 75007  vous accueille dans une ambiance chaleureuse et conviviale. Ouvert tous les jours de 9h à minuit, cet établissement est l\'endroit idéal pour savourer une cuisine de qualité à toute heure de la journée. Pour toute réservation ou information, n\'hésitez pas à joindre l\'équipe du restaurant au 0602877928. Le propriétaire, Max veille à offrir à chaque client une expérience gastronomique exceptionnelle.',
 
     },
     {
       title: 'Pedzouille La Grange',
       img: 'https://www.ahstatic.com/photos/a7l6_rsr003_00_p_2048x1536.jpg',
-      price: 18,
+      price: '',
       description: 'un havre de paix culinaire situé au cœur de la campagne française. Notre restaurant est un hommage aux traditions gastronomiques françaises, où chaque plat est préparé avec soin et passion, en utilisant des ingrédients locaux de saison',
 
     },
     {
       title: 'Les Deux Magots',
       img: 'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/2c/e8/61/b6/caption.jpg',
-      price: 18,
-      description: '',
+      price: '',
+      description: 'statues légendaires perchées sur leur pilier central, observent d’un regard amusé l’effervescence de ce Café littéraire de Saint-Germain-des-Prés',
 
     },
     {
       title: 'Colère',
       img: 'https://th.bing.com/th/id/R.021cfdae38cee0f6d2bdb3428fd7b6b9?rik=Bzjt3oB7%2bHDE2Q&pid=ImgRaw&r=0.jpg',
-      price: 18,
-      description: '',
+      price: '',
+      description: 'notre deuxième pêché, s’installe au cœur du 9 ème à deux pas des emblématiques Folies Bergères. Enflammée, démesurée, relevée',
     },
     {
       title: 'New Soul Food',
       img: 'https://th.bing.com/th/id/R.5eb3718e12f39045aec8dbd46bc86df1?rik=E1qkHiMTk8C5YQ&pid=ImgRaw&r=0.jpg',
-      price: 18,
-      description: '',
+      price: '',
+      description: 'Bienvenue au Maquis, le restaurant de New Soul Food situé dans le 10ème arrondissement à Paris, proche du Canal Saint Martin et du Quai de Jemmapes, de la place Louis Blanc',
     },
     {
       title: 'Bangkok',
       img: 'https://th.bing.com/th/id/OIP.De3COyLmvSPy0oKMhhTRGgHaE8?rs=1&pid=ImgDetMain.jpg',
-      price: 18,
-      description: '',
+      price: '',
+      description: 'Faites-vous livrer par les meilleurs restaurants de votre quartier. Vos plats préférés de Paris livrés chez vous.',
     }
 
   ];
